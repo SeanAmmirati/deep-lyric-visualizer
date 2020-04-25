2ca8b1d635523b6d198c6d8fe20ddfe1db4d6f81
Sean Ammirati <ammirati.sean@gmail.com>
2020-04-25 19:06:11 -0400

    adding markdown support, as I find restructuredtext to be cumbersome

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ import sys
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['autoapi.extension',
+extensions = ['autoapi.extension', 'recommonmark',
               'sphinx.ext.coverage', 'sphinx.ext.napoleon']
 
 
