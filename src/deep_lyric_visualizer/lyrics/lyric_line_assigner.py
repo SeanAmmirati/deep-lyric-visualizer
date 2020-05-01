377f8d9d26eba614a79b1af078662b1497edac9f
Sean Ammirati <sammirati@statsworks.info>
2020-04-30 21:11:35 -0400

    adding documentation to lyric_line_assigner

--- src/deep_lyric_visualizer/lyrics/lyric_line_assigner.py
+++ src/deep_lyric_visualizer/lyrics/lyric_line_assigner.py
@@ -2,16 +2,48 @@ import numpy as np
 import pandas as pd
 
 from deep_lyric_visualizer.lyrics.lyric_weigher import ConeLyricWeigher, EqualLyricWeigher
-from deep_lyric_visualizer.nlptopic_selector import MaxMaxSelector, MeanMaxSelector
-from deep_lyric_visualizer.nlpwordvector_similarity import (CosineWordVectorSimilarity,
-                                                            EuclidWordVectorSimilarity)
+from deep_lyric_visualizer.nlp.topic_selector import MaxMaxSelector, MeanMaxSelector
+from deep_lyric_visualizer.nlp.wordvector_similarity import (CosineWordVectorSimilarity,
+                                                             EuclidWordVectorSimilarity)
 
 
 class LyricLineAssigner:
 
     def __init__(self, weighing_type='eq', similarity_metric='cosine',
                  topic_selector_type='max_max', weighing_obj=None,
-                 similarity_obj=None, topic_selector=None):
+                 similarity_obj=None, topic_selector_obj=None):
+        """A utility that handles the assignment of weights to each lyric in a
+        line, stanza, etc for use in a vectorizer.
+
+        Args:
+            weighing_type (str, optional): the type of weighing to use, if
+                defined by a lyrics.LyricWeigher class.
+                One of {'eq', 'cone', 'first', 'last'}.
+                For ease of use -- just syntactic sugar for passing the
+                appropriate class to weighing_obj argument.
+                Defaults to 'eq', which gives all words an equal weight.
+            similarity_metric (str, optional): defines the similarity
+                between word vectors, as defined by a
+                nlp.WordvectorSimilarity object.
+                One of {'cosine', 'euclid'}.
+                For ease of use -- just syntactic sugar for passing the
+                appropriate class to similarity_obj.
+                Defaults to 'cosine', which uses the cosine distance between
+                the two vectors.
+            topic_selector_type (str, optional): defines the topic selector,
+                as in how to select a topic given a line of lyrics. One of
+                {'mean_max', 'max_max'}. This is defined as a nlp.TopicSelector
+                subclass.
+                For ease of use -- just syntactic sugar for passing the
+                appropriate class to topic_selector.
+                Defaults to 'max_max'.
+            weighing_obj (lyrics.LyricWeigher, optional): A custom LyricWeigher
+                object. Defaults to None.
+            similarity_obj (nlp.WordVectorSimilarity, optional): A custom
+                WordVectorSimilarity object. Defaults to None.
+            topic_selector_obj (nlp.TopicSelector, optional): A custom
+                TopicSelector object. Defaults to None.
+        """
         if similarity_obj:
             self.similarity = similarity_obj
         elif similarity_metric == 'cosine':
@@ -30,14 +62,26 @@ class LyricLineAssigner:
         elif weighing_type == 'last':
             self.weight = EqualLyricWeigher(-1)
 
-        if topic_selector:
-            self.topic_selector = topic_selector
+        if topic_selector_obj:
+            self.topic_selector = topic_selector_obj
         elif topic_selector_type == 'max_max':
             self.topic_selector = MaxMaxSelector()
         elif topic_selector_type == 'mean_max':
             self.topic_selector = MeanMaxSelector()
 
     def assign_line(self, line, candidate_vectors, n=1):
+        """Assigns a line of lyrics to a topic.
+
+        Args:
+            line (list [np.array]): A list of vectors for a line of lyrics
+            candidate_vectors (list [np.array]): A list of candidate vectors
+                to consider (the topic vectors)
+            n (int, optional): The number of topics to return. Defaults to 1.
+
+        Returns:
+            list [int]: A list of indexes representing the appropriate topics
+                that were chosen. (tentative)
+        """
         weights = self.weight.weigh_lyrics(line)
         similarities = pd.DataFrame([self.similarity.calculate_similarities(
             word, candidate_vectors) for word in line])
